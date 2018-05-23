368c9726cf83d490251ed305fcf3bbe1661d1e32
Raimon Esteve <raimon@nan-tic.com>
2018-05-24 00:47:09 +0200

    Branch 4.8

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.pool import Pool
-from .party import Party
+from . import party
 
 def register():
     Pool.register(
-        Party,
+        party.Party,
         module='party_type', type_='model')
